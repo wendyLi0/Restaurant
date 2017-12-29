97e3ec47213bc0ea1f2570264f91b492df168263
liwenwen <liwenwen@weidian.com>
2017-12-29 17:00:16 +0800

    提交最新代码

--- wenwentry/addRes.py
+++ wenwentry/addRes.py
@@ -26,8 +26,11 @@ def create_restaurant():
     if not request.form:
         log.info('添加餐厅请求参数为空')
         abort(400)
-    DB_Operation.add_restaurant(request.form)
-    return jsonify(request.form), 200
+    if (request.form['cantingName'] != "" and request.form['cantingAddress'] != ""):
+        DB_Operation.add_restaurant(request.form)
+        return jsonify(request.form), 200
+    else:
+        return jsonify(request.form), 200
 
 
 @app.errorhandler(404)
@@ -38,7 +41,8 @@ def not_found(error):
 @app.route('/canting/getlist', )
 def get_restaurant():
     data_json = DB_Operation.get_restaurant("true")
-    return jsonify({'restaurant': data_json})
+    restaurants = jsonify({'restaurant': data_json})
+    return restaurants
 
 
 @app.route('/canting/ramdon')
@@ -48,4 +52,4 @@ def get_ramdon_restaurant():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
